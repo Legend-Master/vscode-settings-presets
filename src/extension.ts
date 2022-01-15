6b810bef30b9f8d8eca080f341b5cc6f58d5ff95
Tony <legendmastertony@gmail.com>
2022-01-15 20:29:58 +0800

    Fix preset can't hot reload when config changed

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,57 @@
-import * as vscode from 'vscode'
+import {
+	window,
+	workspace,
+	commands,
+	ExtensionContext,
+	ConfigurationTarget,
+	WorkspaceConfiguration,
+} from "vscode";
 
 import { settingsPresetsProvider, Settings } from './settingsPresetsProvider'
 import { PRESETS_SETTINGS_NAME, VIEW_ID } from "./constants";
 
-export function activate(context: vscode.ExtensionContext) {
-	if (!(vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0)) {
+export function activate(context: ExtensionContext) {
+	if (!(workspace.workspaceFolders && workspace.workspaceFolders.length > 0)) {
 		return
 	}
 
-	const config = vscode.workspace.getConfiguration()
+	let config = workspace.getConfiguration()
 	let presets = {...config.get<any>(PRESETS_SETTINGS_NAME)}
 
 	const windowProvider = new settingsPresetsProvider()
-	vscode.workspace.onDidChangeConfiguration((e) => {
+	workspace.onDidChangeConfiguration((e) => {
 		if (e.affectsConfiguration(PRESETS_SETTINGS_NAME)) {
 			windowProvider.refresh()
+			config = workspace.getConfiguration()
 			presets = {...config.get<any>(PRESETS_SETTINGS_NAME)}
 		}
 	})
-	context.subscriptions.push(vscode.window.registerTreeDataProvider(VIEW_ID, windowProvider))
+	context.subscriptions.push(window.registerTreeDataProvider(VIEW_ID, windowProvider))
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('settingsPresets.applyPreset', async (settingsItem: Settings) => {
-			function updateSettings(configs: vscode.WorkspaceConfiguration, settings: any, field: string) {
+		commands.registerCommand('settingsPresets.applyPreset', async (settingsItem: Settings) => {
+			function updateSettings(configs: WorkspaceConfiguration, settings: any, field: string = '') {
 				for (const configName in settings) {
 					let newField = field + (field !== '' ? '.' : '') + configName
 					const setting = settings[configName]
 					if (typeof setting === 'object') {
 						updateSettings(configs, setting, newField)
 					}
-					config.update(newField, setting, vscode.ConfigurationTarget.Workspace)
+					config.update(newField, setting, ConfigurationTarget.Workspace)
 				}
 			}
-			updateSettings(vscode.workspace.getConfiguration(), presets[settingsItem.label], '')
+			updateSettings(workspace.getConfiguration(), presets[settingsItem.label])
+
 			const openSettings = 'Open Settings File'
-			const choice = await vscode.window.showInformationMessage('Preset: ' + settingsItem.label + ' applied', openSettings)
+			const choice = await window.showInformationMessage('Preset: ' + settingsItem.label + ' applied', openSettings)
 			if (choice === openSettings) {
-				vscode.commands.executeCommand('workbench.action.openWorkspaceSettingsFile')
+				commands.executeCommand('workbench.action.openWorkspaceSettingsFile')
 			}
 		})
 	)
 	context.subscriptions.push(
-		vscode.commands.registerCommand('settingsPresets.addPreset', async () => {
-			const presetName = await vscode.window.showInputBox({
+		commands.registerCommand('settingsPresets.addPreset', async () => {
+			const presetName = await window.showInputBox({
 				placeHolder: 'Preset name'
 			})
 			if (typeof presetName === 'string' && presetName.length !== 0) {
@@ -57,14 +66,14 @@ export function activate(context: vscode.ExtensionContext) {
 					}
 				}
 				presets[presetName] = settingsToSave
-				config.update(PRESETS_SETTINGS_NAME, presets, vscode.ConfigurationTarget.Global)
+				config.update(PRESETS_SETTINGS_NAME, presets, ConfigurationTarget.Global)
 			}
 		})
 	)
 	context.subscriptions.push(
-		vscode.commands.registerCommand('settingsPresets.delectPreset', (settingsItem: Settings) => {
+		commands.registerCommand('settingsPresets.delectPreset', (settingsItem: Settings) => {
 			delete presets[settingsItem.label]
-			config.update(PRESETS_SETTINGS_NAME, presets, vscode.ConfigurationTarget.Global)
+			config.update(PRESETS_SETTINGS_NAME, presets, ConfigurationTarget.Global)
 		})
 	)
 }
